6187fff0a178dbbc785c12b557ff05b9524f89ae
Michał Szczepański <siematumichael@protonmail.com>
2023-05-01 17:29:28 +0200

    Added real world map to application (Map is from google api)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,53 @@
 <script lang="ts">
+    import "./styles.css"; /* import styles which cannot be removed across interpreted as unused by PurgeCSS  */
+    import { PUBLIC_MAP_API_KEY, PUBLIC_MAP_ID } from "$env/static/public";
+    import { onMount } from "svelte";
     import OptionsStripe from "$lib/OptionsStripe.svelte";
     import UpperStripe from "$lib/UpperStripe.svelte";
+    import { Loader } from "@googlemaps/js-api-loader";
 
     let gameCanvas: HTMLCanvasElement;
+
+    async function addMap() {
+        // Example location
+        const position = { lat: 52.237049, lng: 21.017532 };
+
+        // Initialise auth data for Google Maps library
+        const loaded = await new Loader({
+            apiKey: PUBLIC_MAP_API_KEY,
+            version: "weekly"
+        }).load();
+
+        // Load Google Maps Library
+        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+        const mapContainer = document.getElementsByClassName("map")[0];
+        const map = new Map(mapContainer as HTMLElement, {
+            maxZoom: 11,
+            minZoom: 5,
+            center: position,
+            zoom: 8,
+            mapId: PUBLIC_MAP_ID
+        });
+
+        // Add to map container canvas created
+        // FIXME: Canvas elminates possibliity to move/zoom on map
+        /* const canvas = document.createElement("canvas");
+        mapContainer.appendChild(canvas);
+
+        // Assign created canvas as a global vartiable
+        gameCanvas = canvas; */
+    }
+
+    // Must be probably performed in this way because is some way durning puting it into global scope i've got an error caused by 'no "window" object'
+    onMount(() => {
+        addMap();
+    })
 </script>
 
 <div class="app">
     <UpperStripe/>
-    <canvas bind:this={gameCanvas}></canvas>
+    <div class="map">
+    </div>
     <OptionsStripe/>
 </div>
 
@@ -25,7 +65,7 @@
         background-color: whitesmoke;
     }
 
-    canvas {
+    div.map {
         position: absolute;
         top: 50px;
         width: 100%;
