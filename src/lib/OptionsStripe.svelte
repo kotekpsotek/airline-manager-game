fcc9a05308b38eef55f6fbf85eeea720040826f7
Michał Szczepański <siematumichael@protonmail.com>
2023-05-11 20:09:52 +0200

    Added ability to display: planes market and user airline routes list when user click on eligable button from bottom application stripe

--- src/lib/OptionsStripe.svelte
+++ src/lib/OptionsStripe.svelte
@@ -1,17 +1,75 @@
 <script lang="ts">
     import { Store, Plane, FlightInternational, ChartLine } from "carbon-icons-svelte"
+    import PlanesMarket from "$lib/PlanesMarket.svelte";
+    import UserRoutes from "$lib/UserRoutes.svelte";
 
     let iconsSize = 28 as 24 | 32;
+    let statusDisplaying: "planes-market" | "user-routes" | null = null;
+
+    /** Clear application main field element = place where all other components of application will be adding or are added */
+    function clearMainAppFieldFromAnyComponents() {
+        // Reset status what component is actual displaying
+        statusDisplaying = null;
+        
+        // Get application main field what mean get place where all other application components will be spawning
+        const mainField = document.getElementsByClassName("map")[0];
+        
+        // List of childrens located into mainField
+        const mC = mainField.children;
+
+        // Itertate over childrens and remove each
+        for (const children of mC) {
+            children.remove();
+        }
+    }
+
+    /** Create planes market = add it to main application field */
+    function createPlanesMarket(ev: Event) {
+        if (statusDisplaying != "planes-market") {
+            // Clear other components spawned prior
+            clearMainAppFieldFromAnyComponents();
+            
+            // Add planes market when user already is not displaying it
+            statusDisplaying = "planes-market";
+            
+            const planesMarket = new PlanesMarket({
+                target: document.getElementsByClassName("map")[0]
+            });
+        }
+        else {
+            // Destroy planes market component and reset it status when user already is displaying it
+            clearMainAppFieldFromAnyComponents();
+        }
+    }
+
+    /** Spawn component with list of user routes and ability to manage user routes */
+    function createRoutesList(ev: Event) {
+        if (statusDisplaying != "user-routes") {
+            // Clear other components spawned prior
+            clearMainAppFieldFromAnyComponents();
+            
+            // Add planes market when user already is not displaying it
+            statusDisplaying = "user-routes";
+            
+            const userRoutes = new UserRoutes({
+                target: document.getElementsByClassName("map")[0]
+            });
+        }
+        else {
+            // Destroy planes market component and reset it status when user already is displaying it
+            clearMainAppFieldFromAnyComponents();
+        }
+    }
 </script>
 
 <div class="options-stripe">
-    <button id="market" title="market">
+    <button id="market" title="market" on:click={createPlanesMarket}>
         <Store size={iconsSize}/>
     </button>
     <button id="fleet" title="fleet">
         <Plane size={iconsSize}/>
     </button>
-    <button id="routes" title="routes">
+    <button id="routes" title="routes" on:click={createRoutesList}>
         <FlightInternational size={iconsSize}/>
     </button>
     <button id="analisys" title="analisys">
