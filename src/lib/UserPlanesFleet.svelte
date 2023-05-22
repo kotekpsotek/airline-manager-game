18dfa47503de577cc17b2faab2cdde4b27b974ad
Michał Szczepański <siematumichael@protonmail.com>
2023-05-22 20:32:24 +0200

    Added css styles for user fleet component

--- src/lib/UserPlanesFleet.svelte
+++ src/lib/UserPlanesFleet.svelte
@@ -5,7 +5,7 @@
     /** Return formated name whether plane is assigned to any route and when is return formated name of route between points and route id into same string */
     function getAssignationRouteNameForGUI(planeId: string): string {
         const routeWhetherIs = Route.planeIsAssignedToRoute(planeId, $userData!.routes);
-        return routeWhetherIs ? `${routeWhetherIs[1].from.name} - ${routeWhetherIs[1].to.name} (${routeWhetherIs[0]})` : "Not assigned"
+        return routeWhetherIs ? `${routeWhetherIs[1].from.name} - ${routeWhetherIs[1].to.name} (<span style="color: green;">${routeWhetherIs[0]}</span>)` : "Not assigned"
     }
 </script>
 
@@ -23,13 +23,47 @@
                 </tr>
                 {#each $userData?.fleet as fleet_unit, it_id}
                     <tr>
-                        <td>{it_id + 1}</td>
-                        <td>{fleet_unit.planeId}</td>
+                        <td class="no">{it_id + 1}</td>
+                        <td style:color="green">{fleet_unit.planeId}</td>
                         <td>{fleet_unit.airplane_brand} {fleet_unit.airplane_model_name}</td>
-                        <td>{getAssignationRouteNameForGUI(fleet_unit.planeId)}</td>
+                        <td>{@html getAssignationRouteNameForGUI(fleet_unit.planeId)}</td>
                     </tr>
                 {/each}
             </table>
         {/if}
     </div>
 </div>
+
+<style>
+    .planes-fleet-inside {
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 5px;
+    }
+
+    h1, h3 {
+        color: rgb(22, 115, 237);
+    }
+
+    table :is(th, td) {
+        font-size: 15px;
+    }
+
+    table th {
+        min-width: 50px;
+        height: 40px;
+        color: white;
+        background-color: rgb(19, 96, 197);
+    }
+
+    table td {
+        padding: 5px;
+        height: 30px;
+        text-align: center;
+    }
+
+    table td.no {
+        font-weight: 600;
+    }
+</style>
