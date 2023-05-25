94deafc72eab67f5fa66a930d668b9b6c938c5a0
Michał Szczepański <siematumichael@protonmail.com>
2023-05-25 20:14:04 +0200

    Formated account balance to format where each 3 digits which aren't behind dot "." are separated using whitespace character

--- src/lib/UpperStripe.svelte
+++ src/lib/UpperStripe.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
     import { UserAvatarFilled } from "carbon-icons-svelte";
     import { userData as userD } from "$lib/storages/interim";
+
+    /** Format account balance to format in which each 3 digits are separated by whitespace. Example for number: 50000 will be: 50 000 */
+    function formatedAccountBalance() {
+        const step1 = String($userD?.balance.toFixed(2)).split(".");
+        const balanceF = step1[0].match(/\d{3}/g);
+        let balanceSeparated = balanceF!.join(" ");
+
+        return balanceSeparated += step1[1] ? "." + step1[1] : "";
+    }
 </script>
 
 <div class="upper-bar">
     <button id="blank"></button> <!-- Required to create eligable place adjustment for elements -->
-    <button id="account-balance" title="Account balance: 300 000$">
-        <p>{$userD?.balance || 0} $</p>
+    <button id="account-balance" title="Account balance: {$userD ? formatedAccountBalance() : 0}$">
+        <p>{$userD ? formatedAccountBalance() : 0} $</p>
     </button>
     <button id="account" title="Your Account">
         <UserAvatarFilled size={32}/>
